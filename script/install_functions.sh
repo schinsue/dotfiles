63343b1a45c77c02377be78898ea52980b9fc1a2
Sergio Chin-Sue <s.chinsue@gmail.com>
2020-12-22 21:20:26 +0000

    Fix typo

--- script/install_functions.sh
+++ script/install_functions.sh
@@ -20,6 +20,6 @@ function install_zsh_syntax_highlighting {
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
 
-function install_tmp {
+function install_tpm {
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
