4da390573d9d9a70f203e77a02580b57eecb19dc
Sergio Chin-Sue <s.chinsue@gmail.com>
2024-06-26 12:39:47 +0100

    Fix ssh-agent stuff for archlinux, fix yazi tmux conf

--- config/personal.sh
+++ config/personal.sh
@@ -4,3 +4,7 @@ export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
 export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
 
 PROJECT_PARENT_DIRS+=("$HOME/Projects")
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  eval $(keychain --eval id_ed25519 -q)
+fi
